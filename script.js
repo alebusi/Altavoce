aa4b876b831077ed983a0cb76efea89319576af1
alebusi <allebusi@gmail.com>
2020-10-12 19:05:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ $(document).ready(function(){
     $("#testo").load("https://docs.google.com/document/d/e/2PACX-1vTYZ7j14zvmea0tzkWS8wdm54-6gxHFMuATwkWe7xBjW_KvD8zOrKGVpErNOLbkV4d5jEzphBLLWT-u/pub span");
 });
 
-/*
-testoRaw=document.getElementById("testoCompleto").innerHTML; */
+testoRaw=document.getElementById("testoCompleto").innerHTML;
 /* testo=testoRaw.substr(testoRaw.indexOf("#")+1); */
 /* testo=sistemaTesto(testoRaw); */	
-/* document.getElementById("testo").innerHTML=testoRaw; */
+document.getElementById("testo").innerHTML=testoRaw;
 
 function sistemaTesto(testo) {
   testo = testo.replace(/à/ig,"ah");
