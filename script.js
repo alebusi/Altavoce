8365722ac858a95f0efc44c3335f080403d1af17
alebusi <allebusi@gmail.com>
2020-10-12 19:51:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var testo="";
-
+/*
 $(document).ready(function(){
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vTYZ7j14zvmea0tzkWS8wdm54-6gxHFMuATwkWe7xBjW_KvD8zOrKGVpErNOLbkV4d5jEzphBLLWT-u/pub span");
 });
-
+*/
 testoRaw=document.getElementById("testoCompleto").innerHTML;
 /* testo=testoRaw.substr(testoRaw.indexOf("#")+1); */
 testo=sistemaTesto(testoRaw);	
