15f86468ca91a7828f56aef2170a89f505aedc50
alebusi <allebusi@gmail.com>
2020-10-12 11:57:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,19 @@
 var testo="";
 
-var dimensione = ["40","60","80","100","120"];
-var num_car = ["28","19","15","12","10"];
-var ind=0; 
-var tempo = 1200;
-var i=-1; //indice posizione nel testo
-var nc=0; //numero caratteri da visualizzare
-var nc_max=28; //numero massimo caratteri da visualizzare
-var current_string="";
-kd = false;
-
-window.addEventListener("keydown", keypress_handler, false);
-window.addEventListener("keyup", keyup_handler, false);
-
 $(document).ready(function(){
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vRDihSpthOhnhcIbIi7Z1OfArt2KtozYcXuglANaXJk-xDDWgN9AXM6go_HFLoCaufZjWZV5EEKpq_P/pub span");
 });
-
-function keypress_handler(event) {
-  if (event.keyCode == 32) { 
-    if (!this.kd) {
-	this.kd = true;
-        avanti();
-    }
-  }
-}
-
-function keyup_handler(event) {
-  if (event.keyCode == 32) {
-     this.kd = false;
-     resetInt();
-  }
-}
   
-function avanti() {
-    if (i == -1) {
-     testoRaw=document.getElementById("testoCompleto").innerHTML;
-     testo=testoRaw.substr(testoRaw.indexOf("#")+1);	    
-    }	
-    try {clearInterval(myTimer);}
-       catch(err){}
-  
-    myTimer = setInterval(function() {
-       if (i < testo.length-1) {
-         i+=1;
-         r_t = testo[i];
-	 if (r_t == " ") {
-	    current_string = current_string.concat('<span style="color:black;">-</span>'); 
-	 }
-	 else {
-            current_string = current_string.concat(r_t);
-	 }
-         document.getElementById("testo").innerHTML = current_string;
-	 nc+=1;
-         if (nc > nc_max-1) {
-           current_string=""; 
-           nc=0;
-         }
-       }
-       //resetInt();
-	  }, tempo);
-}
+testoRaw=document.getElementById("testoCompleto").innerHTML;
+testo=sistemaTesto(testoRaw);	
+document.getElementById("testo").innerHTML=testo;
 
-function resetInt() {
-    try {clearInterval(myTimer);}
-		catch(err){}
+function sistemaTesto(testo) {
+  testo = testo.replace(/à/ig,"ah");
+  testo = testo.replace(/ì/ig,"ih");
+  testo = testo.replace(/ù/ig,"uh");
+  testo = testo.replace(/ò/ig,"oh");
+  testo = testo.replace(/è/ig,"eh");
+  testo = testo.replace(/é/ig,"eh");
+  return testo;
 }
-
-function cambioVelocita() {
-  tempo -= 100;
-  if (tempo < 500) {
-    tempo=1200;
-  }
-  document.getElementById("velocita").innerHTML = tempo;
-}
-
-function cambioDimensione() {
-  ind+=1;
-  if (ind > dimensione.length-1) ind=0;
-  document.getElementById("testo").style.fontSize=dimensione[ind];
-  document.getElementById("dimensione").innerHTML = dimensione[ind];
-  nc_max=num_car[ind];
-}
-
-function cambioCase() {
-    if (document.getElementById("case").style.textTransform == "lowercase") {
-	 document.getElementById("case").style.textTransform = "uppercase";
-	 document.getElementById("testo").style.textTransform = "uppercase"; 
-    }
-    else {
-	 document.getElementById("case").style.textTransform = "lowercase";
-	 document.getElementById("testo").style.textTransform = "lowercase";
-    }
-}
-
