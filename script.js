95141296f27b284fddec8b91a07710240d0c5246
alebusi <allebusi@gmail.com>
2020-10-13 17:55:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var testo="";
-
+/*
 $(document).ready(function(){
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vRDihSpthOhnhcIbIi7Z1OfArt2KtozYcXuglANaXJk-xDDWgN9AXM6go_HFLoCaufZjWZV5EEKpq_P/pub span");
 });
-  
+*/  
 testoRaw=document.getElementById("testoCompleto").innerHTML;
 /* testo=testoRaw.substr(testoRaw.indexOf("#")+1); */
 /* testo=sistemaTesto(testoRaw); */	
