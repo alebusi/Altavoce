9d8f53139cca0b7c18281897fb7293b53d0c4d36
alebusi <allebusi@gmail.com>
2020-10-13 17:39:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var testo;
-var testoRaw;
+var testo="";
 
 $(document).ready(function(){
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vRDihSpthOhnhcIbIi7Z1OfArt2KtozYcXuglANaXJk-xDDWgN9AXM6go_HFLoCaufZjWZV5EEKpq_P/pub span");
