5bd547382fac41b48973c5394e349dee68937670
alebusi <allebusi@gmail.com>
2020-10-13 14:52:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var testoFinale="";
 
 window.addEventListener("keydown", keypress_handler, false);
 
-$(document).ready(function(){
+/*$(document).ready(function(){*/
     $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vTYZ7j14zvmea0tzkWS8wdm54-6gxHFMuATwkWe7xBjW_KvD8zOrKGVpErNOLbkV4d5jEzphBLLWT-u/pub span");
-});
+/*});*/
 
 function keypress_handler(event) {
   if (event.keyCode == 32) { 
@@ -14,14 +14,14 @@ function keypress_handler(event) {
     document.getElementById("testo").innerHTML=testoFinale;
     }
 }
-
+/*
 $(window).load(function () {
     testoRaw=document.getElementById("testoCompleto").innerHTML;
-    /* testo=testoRaw.substr(testoRaw.indexOf("#")+1); */
+    /* testo=testoRaw.substr(testoRaw.indexOf("#")+1);
     testoFinale=sistemaTesto(testoRaw);	
     document.getElementById("testo").innerHTML=testoFinale;
 });
-
+*/
 /*
 testoRaw=document.getElementById("testoCompleto").innerHTML;
 testo=testoRaw.substr(testoRaw.indexOf("#")+1);
