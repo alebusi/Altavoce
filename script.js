59f5a54d66c241334a5ef6eb1bbb489027b820eb
alebusi <allebusi@gmail.com>
2020-10-13 15:11:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var testoFinale="";
 window.addEventListener("keydown", keypress_handler, false);
 
 $(document).ready(function(){
-    $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vTYZ7j14zvmea0tzkWS8wdm54-6gxHFMuATwkWe7xBjW_KvD8zOrKGVpErNOLbkV4d5jEzphBLLWT-u/pub span");
+    $("#testoCompleto").load("https://docs.google.com/document/d/e/2PACX-1vTYZ7j14zvmea0tzkWS8wdm54-6gxHFMuATwkWe7xBjW_KvD8zOrKGVpErNOLbkV4d5jEzphBLLWT-u/pub span", caricaTesto);
 });
 
 function keypress_handler(event) {
@@ -15,19 +15,21 @@ function keypress_handler(event) {
     }
 }
 
+/*
 $(window).load(function () {
     testoRaw=document.getElementById("testoCompleto").innerHTML;
-    /* testo=testoRaw.substr(testoRaw.indexOf("#")+1);*/
     testoFinale=sistemaTesto(testoRaw);	
     document.getElementById("testo").innerHTML=testoFinale;
 });
-
-/*
-testoRaw=document.getElementById("testoCompleto").innerHTML;
-testo=testoRaw.substr(testoRaw.indexOf("#")+1);
-testoFinale=sistemaTesto(testoRaw);	
-document.getElementById("testo").innerHTML=testoFinale;
 */
+
+function caricaTesto() {
+  testoRaw=document.getElementById("testoCompleto").innerHTML;
+  /* testo=testoRaw.substr(testoRaw.indexOf("#")+1); */
+  testoFinale=sistemaTesto(testoRaw);	
+  document.getElementById("testo").innerHTML=testoFinale;
+}
+
 function sistemaTesto(testo) {
   testo = testo.replace(/à/ig,"ah");
   testo = testo.replace(/ì/ig,"ih");
